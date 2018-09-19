0c75a437b49303794ee04a6fa52cf21aa40fe4f3
anett6 <anettpelyva6@gmail.com>
2018-09-19 21:32:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/helloandroid2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.helloandroid2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onlineCourses(View view) {
+        Intent browserIntent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.udacity.com/pathfinder"));
+        startActivity (browserIntent);
+    }
+}
